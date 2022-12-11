2ec683996f6ab1f6a35b99e44a4e34e4713996d0
leekyukin <leekujin14@gmail.com>
2022-12-11 15:27:16 +0900

    set: initial

---
+++ hexagonal-example/src/test/java/com/playground/hexagonalexample/HexagonalExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playground.hexagonalexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
